733995ff04bd6153b8864d6711d332418fda3eda
Vlad <vlad@MacBook-Pro-Vladislav.local>
2013-01-25 22:24:25 +0700

    Added empty cocos2d project.

---
+++ taptapper/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  taptapper
+//
+//  Created by Vlad on 25.01.13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
